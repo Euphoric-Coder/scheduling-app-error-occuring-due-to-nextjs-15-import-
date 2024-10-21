0eab79050014180f4eedc6d8040aecb3b93213ae
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2024-10-22 00:36:19 +0530

    Set up of the Authentication using Clerk is successful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const page = () => {
+export default function HomePage() {
+  const { userId } = auth();
+  if (userId != null) redirect("/events");
+
   return (
-    <div className='text-center container my-4 mx-auto'>
-      <h1 className='text-3xl mb-4 font-bold'>Home Page</h1>
-      <div className="flex gap-2 justify-center">
-        <Button>Signup</Button>
-        <Button>Signin</Button>
-
+    <div className="text-center container my-4 mx-auto">
+      <h1 className="text-3xl mb-4">Fancy Home Page</h1>
+      <div className="flex gap-5 justify-center">
+        <Button asChild>
+          <SignInButton />
+        </Button>
+        <Button asChild>
+          <SignUpButton />
+        </Button>
+        <UserButton />
       </div>
     </div>
-  )
+  );
 }
-
-export default page
